b25f6c21a56a4534ced06a52bb7a8872187ecb24
dhruv3 <dhruv.mnit@gmail.com>
2018-10-24 20:30:10 -0400

    Adding a component inside a component and setting up its template using angular-material

--- week1/angular-wwek1-init/conFusion/src/app/app.module.ts
+++ week1/angular-wwek1-init/conFusion/src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ week1/angular-wwek1-init/conFusion/src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+  id: string;
+  name: string;
+  image: string;
+  category: string;
+  featured: string;
+  label: string;
+  price: string;
+  description: string;
+}
