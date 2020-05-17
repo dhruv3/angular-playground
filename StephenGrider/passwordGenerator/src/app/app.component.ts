72d32da5a2e2755161fe9501bc105ba7b1f54239
dhruv3 <dhruv.mnit@gmail.com>
2020-05-17 15:12:21 -0500

    Adding func and styling

--- StephenGrider/passwordGenerator/src/app/app.component.ts
+++ StephenGrider/passwordGenerator/src/app/app.component.ts
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'passwordGenerator';
+  password = "";
+  includeLetters = false;
+  includeSymbols = false;
+  includeNumbers = false;
+  length = 0;
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeLength(value: string){
+    this.length = parseInt(value);
+  }
 }
