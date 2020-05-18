6303318eb994b6f13337a8337fea636ea62a00cd
dhruv3 <dhruv.mnit@gmail.com>
2020-05-18 10:19:08 -0500

    initial commit for typing proj

---
+++ StephenGrider/typing-challenge/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'typing-challenge';
+}
