02a85c78359029c48544c514fee9887d5c7afaa6
dhruv3 <dhruv.mnit@gmail.com>
2020-05-17 22:42:00 -0500

    Adding for-loop to create elems

--- StephenGrider/cards/src/app/card/card.component.ts
+++ StephenGrider/cards/src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-
+  @Input() title = "";
+  @Input() imageUrl = "";
+  @Input() content = "";
+  @Input() username = "";
+  
   constructor() { }
 
   ngOnInit(): void {
