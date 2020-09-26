c3b24bdc49b3826ce39f75499e1a781699eab7c6
dhruv3 <dhruv.mnit@gmail.com>
2020-09-26 15:16:19 -0500

    Adding custom directive

--- StephenGrider/pages-directives/src/app/class.directive.ts
+++ StephenGrider/pages-directives/src/app/class.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
+  constructor(private element:ElementRef) { }
 
-  constructor() { }
-
+  @Input('appClass') set backgroundColor(color: string){
+    this.element.nativeElement.style.backgroundColor = color;
+  }
 }
